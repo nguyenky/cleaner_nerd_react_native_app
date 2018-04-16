5dfa28b41242943d63444063e9da55988cd825cf
nguyenky <lenguyenky1801@gmail.com>
2018-04-16 10:00:29 +0700

    done login UI

--- components/auths/Login.js
+++ components/auths/Login.js
@@ -3,46 +3,121 @@ import{
   View,
   Text,
   StyleSheet,
-  Image
+  Image,
+  ImageBackground,
+  Button,
+  Alert,
+  TouchableHighlight,
+  TextInput,
 } from 'react-native';
-const remote = 'https://s3.envato.com/files/243549096/Clock%20Background%2001_preview1.JPG';
+import PropTypes from 'prop-types';
+import Wallpaper from './Wallpaper';
+import Logo from './Logo';
 export default class Login extends Component{
   static navigationOptions = {
     header: null,
     title: 'Welcome to Cleaning nerd !!!',
   };
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+    }
+  }
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
   render(){
-    const resizeMode = 'center';
-    const text = 'This is some text inlaid in an image';
     return(
-      <View style={styles.container}>
-         <View style={styles.loginContainer}>
-            <Image
-                  resizeMode="contain"
-                   source={{uri: 'https://facebook.github.io/react/logo-og.png'}}
-                   style={styles.logo} />
-         </View>
-        <View style={styles.formContainer}>
-          <Text>form</Text>
-        </View>
-       </View>
+      <ImageBackground
+          source={{uri: 'https://raw.githubusercontent.com/dwicao/react-native-login-screen/master/src/images/wallpaper.png'}}
+          style={styles.picture}>
+          {this.props.childen}
+          <View style={styles.container}>
+            <View style={styles.inputContainer}>
+              <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+              <TextInput style={styles.inputs}
+                  placeholder="Email"
+                  keyboardType="email-address"
+                  underlineColorAndroid='transparent'
+                  onChangeText={(email) => this.setState({email})}/>
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+              <TextInput style={styles.inputs}
+                  placeholder="Password"
+                  secureTextEntry={true}
+                  underlineColorAndroid='transparent'
+                  onChangeText={(password) => this.setState({password})}/>
+            </View>
+
+            <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+              <Text style={styles.loginText}>Login</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+                <Text>Forgot your password?</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+                <Text>Register</Text>
+            </TouchableHighlight>
+          </View>
+      </ImageBackground >
     )
   };
 }
 
 const styles = StyleSheet.create({
+  picture: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
   container: {
-      flex: 1,
-      backgroundColor: '#2c3e50',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0095ff00',
   },
-  loginContainer:{
-      alignItems: 'center',
-      flexGrow: 1,
-      justifyContent: 'center'
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
   },
-  logo: {
-      position: 'absolute',
-      width: 300,
-      height: 100
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
   }
 });
